a65c23800c591f148bac73159f7b91ca0c14fcbd
Oriana <orianasofia86@gmail.com>
2022-07-28 19:53:53 -0400

    listo el put

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,109 +1,136 @@
-import React , {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 
 
 //create your first component
 const Home = () => {
 
-	const [task,setTask] = useState (
-		{ label: "",
-		isDone: false
+	const [task, setTask] = useState(
+		{
+			label: "",
+			done: false
 		}
 	)
-	const [listTask, setListTask] = useState ([])
+	const [listTask, setListTask] = useState([])
 
 	const urlBase = "https://assets.breatheco.de/apis/fake/todos/user"
-	const user = "fernando"
+	const user = "oriana"
 
 	const handleChange = (event) => {
-		 setTask({...task,[event.target.name]: event.target.value})
-		
+		setTask({ ...task, [event.target.name]: event.target.value })
+
 	}
 
-	const saveListTask =(event)=> {
-		 
-		 if (event.key === "Enter"){
-			setListTask([...listTask, task])
-			setTask({task:"", isDone:false})
-		 }
+	const saveListTask = async (event) => {
+
+		if (event.key === "Enter") {
+			if (task.label.trim() !== "") {
+				// si es distinto de vacion es xq tiene tarea
+				try {
+					let response = await fetch(`${urlBase}/${user}`, {
+						method: "PUT",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify([...listTask, task])
+					})
+					if (response.ok) {
+						setTask({ label: "", done: false })
+						getTodos()
+					}
+
+				} catch (error) {
+					console.log(error)
+				}
+
+			}
+		}
+
 
 	};
 
-	
-	const deleteTask =(id)=>{
-		
-		let newListTask = listTask.filter((item,index)=>{
+
+	const deleteTask = (id) => {
+
+		let newListTask = listTask.filter((item, index) => {
 			return (
-				id != index 
+				id != index
 			)
 		})
-        setListTask(newListTask)
+		setListTask(newListTask)
 	}
 
-const getTodos=async()=>{
-		try{
+
+
+
+	const getTodos = async () => {
+		try {
 			let response = await fetch(`${urlBase}/${user}`)
 			let data = await response.json()
-			if (response.status !== 404){
+			if (response.status !== 404) {
 				setListTask(data)
-				
-			}else {
-				let responseTodos = await fetch(`${urlBase}/${user}` , {
-					method:"POST", headers:{ "Content-Type":"application/json"}, body: JSON.stringify([])
-				
-			})
 
-           if (responseTodos.ok){
-			getTodos()}
-		}
+			} else {
+				let responseTodos = await fetch(`${urlBase}/${user}`, {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify()
 
-		} catch(error){ 
+				})
+
+
+				if (responseTodos.ok) {
+					getTodos()
+				}
+			}
+
+		} catch (error) {
 			console.log(`Explote : ${error}`)
 
 		}
 	}
 
-	useEffect(()=> {
+	useEffect(() => {
 		getTodos()
-	},[])
+	}, [])
 
 	return (
 		<div className="container">
-			
+
 			<div className="row"> <h1 className="titulo">todos</h1></div>
 			<div className="row note">
-				
-				
-			    <div className="row">
-				    <div className="col-12 col-md-6 divInput">
-					    <input className="input" 
-					    name="task" value={task.task} 
-				     onChange={handleChange} type="text" 
-				     placeholder="What needs to be done?"
-				     onKeyDown={saveListTask}/>
-				    </div>	
-			    </div>
-			    <div className="row">
-				    <div className="col col-12 col-md-6 divUl">
-					    <ul>
-							    {listTask.map((item,index)=>{
-								    return(
-									<li className="li" key={index} onClick={()=>deleteTask(index)} >
+
+
+				<div className="row">
+					<div className="col-12 col-md-6 divInput">
+						<input className="dato"
+							name="label" 
+							value={task.label}
+							onChange={handleChange} 
+							type="text"
+							placeholder="What needs to be done?"
+							onKeyDown={saveListTask} />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col col-12 col-md-6 divUl">
+						<ul>
+							{listTask.map((item, index) => {
+								return (
+									<li className="li" key={index} onClick={() => deleteTask(index)} >
 										<div className="divLoop d-flex">
-											<div className="textoLi">{item.label} </div> 
-										    <div className="iconoLi"><i className="fas fa-times"></i></div> 
+											<div className="textoLi">{item.label} </div>
+											<div className="iconoLi"><i className="fas fa-times"></i></div>
 										</div>
 									</li>
-								    )
-							    })}
-					    </ul>	
-					<p className="botton">{listTask.length}  item left</p>
-				    </div>
-					
-			    </div>
-                
+								)
+							})}
+						</ul>
+						<p className="botton">{listTask.length}  item left</p>
+					</div>
+
+				</div>
+
 			</div>
-			
+
 
 
 		</div>
